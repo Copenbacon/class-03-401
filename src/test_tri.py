8cc44e44b7a7bcec8f12e96e889e07b787db625d
copenbacon <sclary50@gmail.com>
2016-12-07 15:56:09 -0800

    more test

--- src/test_tri.py
+++ src/test_tri.py
@@ -1,7 +1,7 @@
 """This tests the Trigrams module."""
 import pytest
 
-PARAMS_TABLE = [
+DICTIONARY_TABLE = [
     [["holy", "shit", "it's", "butter"], {
         'holy shit': ['it\'s'],
         'shit it\'s': ['butter'],
@@ -13,9 +13,35 @@ PARAMS_TABLE = [
     }],
 ]
 
+DATA_LIST_TABLE = [
+    ["src/texts/simple_test.txt", [
+        "holy",
+        "shit,",
+        "it's",
+        "fucking",
+        "butter"]],
+    ["src/texts/simple_test2.txt", [
+        "pride",
+        "and",
+        "prejudice",
+        "is",
+        "our",
+        "book",
+        "of",
+        "choice"]]
 
-@pytest.mark.parametrize("data_list, result", PARAMS_TABLE)
+]
+
+
+@pytest.mark.parametrize("data_list, result", DICTIONARY_TABLE)
 def test_iter_over_data(data_list, result):
-    """Test the main function from Trigrams."""
+    """Test the iter_over_data_list function from Trigrams."""
     from trigrams import iter_over_data_list
     assert iter_over_data_list(data_list) == result
+
+
+@pytest.mark.parametrize("path, result", DATA_LIST_TABLE)
+def test_setup(path, result):
+    """Test the setup function from Trigrams."""
+    from trigrams import setup
+    assert setup(path) == result
