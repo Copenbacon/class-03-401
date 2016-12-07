51a800edea45b649d51a5fe2a1cd7eb45dffbc17
copenbacon <sclary50@gmail.com>
2016-12-07 15:34:20 -0800

    tests

--- src/test_tri.py
+++ src/test_tri.py
@@ -2,12 +2,20 @@
 import pytest
 
 PARAMS_TABLE = [
-    [],
+    [["holy", "shit", "it's", "butter"], {
+        'holy shit': ['it\'s'],
+        'shit it\'s': ['butter'],
+    }],
+    [["holy", "shit", "it's", "fucking", "butter"], {
+        'holy shit': ['it\'s'],
+        'shit it\'s': ['fucking'],
+        'it\'s fucking': ['butter']
+    }],
 ]
 
 
-@pytest.mark.parametrize("path, num_words, result", PARAMS_TABLE)
-def test_main(path, num_words, result):
+@pytest.mark.parametrize("data_list, result", PARAMS_TABLE)
+def test_iter_over_data(data_list, result):
     """Test the main function from Trigrams."""
-    from trigrams import main
-    assert main(path, num_words) == result
+    from trigrams import iter_over_data_list
+    assert iter_over_data_list(data_list) == result
