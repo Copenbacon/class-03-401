79bf571e8e76af4644f21413e7c870a125539dfb
copenbacon <sclary50@gmail.com>
2016-12-08 00:09:13 -0800

    refactoring for more tests and simpler prettier code

--- src/test_tri.py
+++ src/test_tri.py
@@ -1,18 +1,11 @@
-
- ```"""This tests the Trigrams module."""
+"""This tests the Trigrams module."""
 import pytest
 
-DICTIONARY_TABLE = [
-    [["holy", "shit", "it's", "butter"], {
-        'holy shit': ['it\'s'],
-        'shit it\'s': ['butter'],
-    }],
-    [["holy", "shit", "it's", "fucking", "butter"], {
-        'holy shit': ['it\'s'],
-        'shit it\'s': ['fucking'],
-        'it\'s fucking': ['butter']
-    }],
+MAIN_TABLE = [
+    ['src/texts/sample.txt', 400, 400],
+    ['src/texts/pride_prejudice.txt', 250, 250],
 ]
+
 
 DATA_LIST_TABLE = [
     ["src/texts/simple_test.txt", [
@@ -32,23 +25,57 @@ DATA_LIST_TABLE = [
         "choice"]],
 ]
 
-GENERATE_TABLE = [
+DICTIONARY_TABLE = [
+    [["holy", "shit", "it's", "butter"], {
+        'holy shit': ['it\'s'],
+        'shit it\'s': ['butter'],
+    }],
+    [["holy", "shit", "it's", "fucking", "butter"], {
+        'holy shit': ['it\'s'],
+        'shit it\'s': ['fucking'],
+        'it\'s fucking': ['butter']
+    }],
+]
+
+FIRST_VALUES_TABLE = [
     [{
+        'holy shit': ['it\'s'],
+    }, ["holy", "shit", "it's"]
+    ],
+
+    [{
+        'shit it\'s': ['butter'],
+    }, ["shit", "it's", "butter"]
+    ],
+
+    [{
+        'it\'s fucking': ['butter'],
+    }, ["it's", "fucking", "butter"]
+    ],
+]
+
+GENERATE_TEXT_TABLE = [
+    [["shit", "it's", "butter"], {
         'holy shit': ['it\'s'],
         'shit it\'s': ['butter'],
     }, 5],
-    [{
+    [["holy", "shit", "it's"], {
         'holy shit': ['it\'s'],
         'shit it\'s': ['butter'],
     }, 10],
 ]
 
+WRITE_SCREEN_TABLE = [
+    [["holy", "shit", "it's", "butter"], "holy shit it's butter "],
+    [["holy", "shit", "it's", "fucking", "butter"], "holy shit it's fucking butter "],
+]
 
-@pytest.mark.parametrize("data_list, result", DICTIONARY_TABLE)
-def test_iter_over_data(data_list, result):
-    """Test the iter_over_data_list function from Trigrams."""
-    from trigrams import iter_over_data_list
-    assert iter_over_data_list(data_list) == result
+
+@pytest.mark.parametrize("path, num_words, result", MAIN_TABLE)
+def test_main(path, num_words, result):
+    """Test the setup function from Trigrams."""
+    from trigrams import main
+    assert len(main(path, num_words).split()) == result
 
 
 @pytest.mark.parametrize("path, result", DATA_LIST_TABLE)
@@ -58,8 +85,29 @@ def test_setup(path, result):
     assert setup(path) == result
 
 
-@pytest.mark.parametrize("special_dict, num_words", GENERATE_TABLE)
-def test_generate_text(special_dict, num_words):
+@pytest.mark.parametrize("data_list, result", DICTIONARY_TABLE)
+def test_iter_over_data(data_list, result):
+    """Test the iter_over_data_list function from Trigrams."""
+    from trigrams import iter_over_data_list
+    assert iter_over_data_list(data_list) == result
+
+
+@pytest.mark.parametrize("some_dict, result", FIRST_VALUES_TABLE)
+def test_generate_first_values(some_dict, result):
+    """Test Generate First Values func is outputting first values."""
+    from trigrams import generate_first_values
+    assert(generate_first_values(some_dict)) == result
+
+
+@pytest.mark.parametrize("text, special_dict, num_words", GENERATE_TEXT_TABLE)
+def test_generate_text(text, special_dict, num_words):
     """Test Generate Text function is outputting correct num_words."""
     from trigrams import generate_text
-    assert len(generate_text(special_dict, num_words)) == num_words
+    assert len(generate_text(text, special_dict, num_words)) == num_words
+
+
+@pytest.mark.parametrize("text, result", WRITE_SCREEN_TABLE)
+def test_write_text_to_screen(text, result):
+    """Test that text should write to screen."""
+    from trigrams import write_text_to_screen
+    assert write_text_to_screen(text) == result
--- src/trigrams.py
+++ src/trigrams.py
@@ -5,8 +5,20 @@ def main(path, num_words):
     """Produce Trigrams from a path with specified number of word."""
     data_list = setup(path)
     dict_of_pp = iter_over_data_list(data_list)
-    text = generate_text(dict_of_pp, num_words)
-    return text
+    text = generate_first_values(dict_of_pp)
+    text = generate_text(text, dict_of_pp, num_words)
+    printme = write_text_to_screen(text)
+    return printme
+
+
+def setup(path):
+    """Setup the file to render a list of words from the file in the path."""
+    import io
+    file = io.open(path, encoding='utf8')
+    data = file.read()
+    file.close()
+    data_list = data.split()
+    return data_list
 
 
 def iter_over_data_list(data_list):
@@ -19,18 +31,8 @@ def iter_over_data_list(data_list):
     return dict_of_pp
 
 
-def setup(path):
-    """Setup the file to render a data list of the words from the path entered."""
-    import io
-    file = io.open(path, encoding='utf8')
-    data = file.read()
-    file.close()
-    data_list = data.split()
-    return data_list
-
-
-def generate_text(dict_of_pp, num_words):
-    """Generate the text to write to the file."""
+def generate_first_values(dict_of_pp):
+    """Generate the first key and values to write to the file."""
     import random
     text = []
     rand_key = random.choice(list(dict_of_pp.keys()))
@@ -39,6 +41,12 @@ def generate_text(dict_of_pp, num_words):
     rand_num = random.randint(0, len(dict_of_pp[rand_key]) - 1)
     new_word = dict_of_pp[rand_key][rand_num]
     text.append(new_word)
+    return text
+
+
+def generate_text(text, dict_of_pp, num_words):
+    """Generate the text to write to the file."""
+    import random
     while len(text) < num_words:
         try:
             new_word_pair = text[-2] + ' ' + text[-1]
@@ -49,6 +57,15 @@ def generate_text(dict_of_pp, num_words):
             new_word_pair = random.choice(list(dict_of_pp.keys()))
             text.append(new_word_pair)
     return text
+
+
+def write_text_to_screen(text):
+    """Write the text to the user's screen."""
+    the_written_word = ''
+    for i in range(len(text)):
+        the_written_word += text[i] + ' '
+    return the_written_word
+
 
 if __name__ == "__main__":
     import sys
